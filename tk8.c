97a19a9aea58abe02d723db9dd03292d7e37dc52
AleXa-crypt <fatmaalexamustofa@gmail.com>
2026-05-02 23:58:29 +0700

    Update tk8.c
    added functions

--- tk8.c
+++ tk8.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 
-int Addition(int a, int b) {
+int Addition(int a, int b)
+{
     int hasil;
-    //fungsi penjumlahan
+    hasil = a + b;
     return hasil;
 }
 
-int Subtraction(int a, int b) {
+int Subtraction(int a, int b)
+{
     int hasil;
-    //fungsi pengurangan
+    hasil = a - b;
     return hasil;
 }
 
-int Multiplication(int a, int b) {
+int Multiplication(int a, int b)
+{
     int hasil;
-    //fungsi perkalian
+    hasil = a * b;
     return hasil;
 }
 
-int Subtraction(int a, int b) {
+int Subtraction(int a, int b)
+{
     int hasil;
-    //fungsi pembagian
+    float hasil = a / b;
     return hasil;
 }
 
-int main(void) {
+int main(void)
+{
     printf("=== Welcome to Calculator Kelompok-10 ===\n");
     printf("Menu:\n");
     printf("1. Addition\n");
@@ -36,49 +41,50 @@ int main(void) {
     printf("Masukkan pilihan anda (1-4): ");
     scanf("%d", &n);
 
-    switch (n) {
-        case 1: 
-            printf("Masukkan 2 angka yang ingin dijumlahkan (addition): ");
-            int a, b;
-            printf("Angka ke-1: ");
-            scanf("%d", &a);
-            printf("Angka ke-2: ");
-            scanf("%d", &b);
-            int hasil = Addition(a, b);
-            printf("Hasil penjumlahnnya adalah %d", hasil);
-            break;
-        case 2:
-            printf("Masukkan 2 angka yang ingin dikurangi (subtraction): ");
-            int a, b;
-            printf("Angka ke-1: ");
-            scanf("%d", &a);
-            printf("Angka ke-2: ");
-            scanf("%d", &b);
-            int hasil = Subtraction(a, b);
-            printf("Hasil pengurangannya adalah %d", hasil);
-            break;
-        case 3:
-            printf("Masukkan 2 angka yang ingin dikalikan (multiplication): ");
-            int a, b;
-            printf("Angka ke-1: ");
-            scanf("%d", &a);
-            printf("Angka ke-2: ");
-            scanf("%d", &b);
-            int hasil = Multiplication(a, b);
-            printf("Hasil perkaliannya adalah %d", hasil);
-            break;
-        case 4:
-            printf("Masukkan 2 angka yang ingin dibagi (division): ");
-            int a, b;
-            printf("Angka ke-1: ");
-            scanf("%d", &a);
-            printf("Angka ke-2: ");
-            scanf("%d", &b);
-            int hasil = Division(a, b);
-            printf("Hasil pembagiannya adalah %d", hasil);
-            break;
-        default: 
-            printf("Input invalid!\n");
+    switch (n)
+    {
+    case 1:
+        printf("Masukkan 2 angka yang ingin dijumlahkan (addition): ");
+        int a, b;
+        printf("Angka ke-1: ");
+        scanf("%d", &a);
+        printf("Angka ke-2: ");
+        scanf("%d", &b);
+        int hasil = Addition(a, b);
+        printf("Hasil penjumlahnnya adalah %d", hasil);
+        break;
+    case 2:
+        printf("Masukkan 2 angka yang ingin dikurangi (subtraction): ");
+        int a, b;
+        printf("Angka ke-1: ");
+        scanf("%d", &a);
+        printf("Angka ke-2: ");
+        scanf("%d", &b);
+        int hasil = Subtraction(a, b);
+        printf("Hasil pengurangannya adalah %d", hasil);
+        break;
+    case 3:
+        printf("Masukkan 2 angka yang ingin dikalikan (multiplication): ");
+        int a, b;
+        printf("Angka ke-1: ");
+        scanf("%d", &a);
+        printf("Angka ke-2: ");
+        scanf("%d", &b);
+        int hasil = Multiplication(a, b);
+        printf("Hasil perkaliannya adalah %d", hasil);
+        break;
+    case 4:
+        printf("Masukkan 2 angka yang ingin dibagi (division): ");
+        int a, b;
+        printf("Angka ke-1: ");
+        scanf("%d", &a);
+        printf("Angka ke-2: ");
+        scanf("%d", &b);
+        int hasil = Division(a, b);
+        printf("Hasil pembagiannya adalah %d", hasil);
+        break;
+    default:
+        printf("Input invalid!\n");
     }
 
     return 0;
